fced3c52bac0b0cdcd4f60ed826e01da6c42ff2c
Lorman <lormanrg@gmail.com>
2024-09-06 02:15:28 -0400

    project set up successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-meadi-app'),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
