bc7dc3543c9b4ee548eb944200ac0a776ca7af2c
Lorman <lormanrg@gmail.com>
2024-09-06 02:56:08 -0400

    entities and validations completed

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  userName: string;
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @IsString()
+  @MinLength(5)
+  password: string;
+
+  @IsString()
+  role: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+    required: true,
+  })
+  userName: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    enum: ['Admin', 'Creator', 'Reader'],
+    required: true,
+  })
+  role: string;
+}
+
+export const UsersSchema = SchemaFactory.createForClass(User);
