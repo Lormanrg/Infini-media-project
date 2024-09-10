35914061332cfade8c4b201720c31c04d851a907
Lorman <lormanrg@gmail.com>
2024-09-09 22:02:51 -0400

    Authentication method completed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { jwtConstants } from './constants/jwt.constant';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '5m' },
     }),
   ],
   controllers: [AuthController],
